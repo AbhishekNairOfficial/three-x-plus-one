b1f836deb7bee4fd102f5f5278aabbbb71269b0d
Abhishek P <abhishek.p@piktorlabs.com>
2021-07-30 23:26:28 +0530

    FIrst commit

---
+++ pages/api/getdata.js
@@ -0,0 +1,34 @@
+export default (req, res) => {
+	const numberFunction = (numberInput) => {
+		const loopDdeLoop = (input) => {
+			let number = input
+			const theNumberIsOdd = number & 1
+			if (theNumberIsOdd) {
+				// If number is odd
+				// Multiply by 3 and add 1
+				number = number * 3 + 1
+			} else {
+				// If number is even
+				// divide by 2
+				number = number / 2
+			}
+			// If the new number is higher than the previous
+			// Update the highest number count
+			if (number >= highestCount) {
+				highestCount = number
+			}
+			return [number, highestCount]
+		}
+
+		let number = numberInput
+		let count = 0
+		let highestCount = number
+		while (number !== 4) {
+			count++
+			;[number, highestCount] = loopDdeLoop(number)
+		}
+		return number
+	}
+
+	res.status(200).json({ value: 'saala' })
+}
